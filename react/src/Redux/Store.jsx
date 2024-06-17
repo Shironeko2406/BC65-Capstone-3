ee8defd3168b826eb18d7de1d37bbb1a204365b2
thanhtri <thothanhtri1109@gmail.com>
2024-06-17 22:56:43 +0700

    update logic api login,register and start build profile

--- react/src/Redux/Store.jsx
+++ react/src/Redux/Store.jsx
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import userReducer from "./Reducers/userReducer";
 export const store = configureStore({
   reducer: {
     number: (state = 1) => state,
+    userReducer,
   },
 });
