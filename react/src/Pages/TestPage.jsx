82c1b984942eaa0c2dd899d953daf51ed3505582
Shironeko2406 <hieuntse161485@fpt.edu.vn>
2024-06-19 09:39:13 +0700

    abc

---
+++ react/src/Pages/TestPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TestPage = () => {
+  return (
+    <div>TestPage
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque veniam maxime neque molestias enim delectus sed dolor hic libero. Veritatis expedita ullam aut quod repudiandae rem? Accusantium quo ad ducimus!</p>
+    </div>
+  )
+}
+
+export default TestPage
